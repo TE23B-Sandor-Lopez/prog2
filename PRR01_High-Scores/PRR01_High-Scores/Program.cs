55a6116d39ef83babcdfd86404a7c3869106c7fc
sandor.lopez <sandor.lopez@elev.ga.ntig.se>
2025-09-05 13:02:46 +0200

    jfaripjårg

---
+++ PRR01_High-Scores/PRR01_High-Scores/Program.cs
@@ -0,0 +1,18 @@
+﻿
+List<string> scores = [];
+
+for (int i = 0; i < 3; i++)
+{
+    Console.WriteLine("Skriv namn på en polare:");
+    string name = Console.ReadLine();
+
+    Console.WriteLine("Poängsätt din polare:");
+    string points = Console.ReadLine();
+    scores.Add(points + " " + name);
+}
+
+foreach (string score in scores)
+{
+    Console.WriteLine(score);
+}
+Console.ReadLine();
